c6cd3cff6387a9f14a628dbc7548081ffa30ffce
MatiasFontalba <ma.fontalba@duocuc.cl>
2025-12-02 19:07:07 -0300

    Informacion de cuenta

--- Proyecto/PetMatch/app/home/(tabs)/cuenta.js
+++ Proyecto/PetMatch/app/home/(tabs)/cuenta.js
@@ -1,9 +1,22 @@
 import { View, Text } from "react-native";
+import { useUserStore } from "../../../stores/useUserStore";
+import { useEffect, useState } from "react";
 
 export default function CuentaScreen() {
+  const { user, logout, getUserById } = useUserStore();
+  const [userInfo, setUserInfo] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      const fetchedUser = await getUserById(user?.uid);
+      console.log(fetchedUser);
+      setUserInfo(fetchedUser);
+    }
+    fetchData();
+  }, []);
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Pantalla de Configuración</Text>
+      <Text>Pantalla</Text>
     </View>
   );
 }
